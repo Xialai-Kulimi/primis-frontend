d0508c0f2d7d935844a0be304bc40a0ee02cea7a
Kulimi <isilkoodarze@gmail.com>
2022-11-04 14:48:02 +0800

    Update src/App.js
    Move color switch to NavBar

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import {
   Grid,
   theme,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
 
 function App() {
@@ -17,7 +16,7 @@ function App() {
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
+          
           <VStack spacing={8}>
             <Logo h="40vmin" pointerEvents="none" />
             <Text>
