81d76cc4d5b67ad49d88ededff4feafbc3396dc5
Kulimi <isilkoodarze@gmail.com>
2022-11-04 14:50:38 +0800

    Update src/index.js
    Add NavBar

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import { ColorModeScript } from '@chakra-ui/react';
+import { ColorModeScript, ChakraProvider } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
+import NavBar from './components/NavBar';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
+
+
 root.render(
   <StrictMode>
+    <ChakraProvider>
     <ColorModeScript />
+
+    {/* <Text
+  bgGradient="linear(to-l, #7928CA, #FF0080)"
+  bgClip="text" // background-clip: text
+  fontSize="6xl" // font-size: theme.fontSizes.6xl => 3.75rem
+  fontWeight="extrabold" // font-weight: theme.fontWeights.extrabold => 800
+>
+  Welcome to Chakra UI
+</Text> */}
+    <NavBar/>
     <App />
+    </ChakraProvider>
+    
   </StrictMode>
 );
 
