a2d26fb5c1f48513596b86972e37fa1b21c072dc
Kulimi <isilkoodarze@gmail.com>
2022-11-04 19:59:28 +0800

    Update src/theme.ts
    update theme

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -3,16 +3,33 @@ import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
   palette: {
-    // type: 'dark',
+    // type: 'dark',s
     primary: {
       main: '#ff5252',
+      light: '#ff7474',
+      dark: '#b23939',
+      contrastText: '#ffffff',
     },
     secondary: {
-      main: '#181818',
+      main: '#3b3b3b',
+      light: '#626262',
+      dark: '#292929',
+      contrastText: '#ffffff',
     },
     background: {
-      default: '#212121',
-      paper: '#303030',
+      default: '#121212',
+      paper: '#181818',
+    },
+    text: {
+      primary: '#dddddd',
+      secondary: 'rgba(255,255,255,0.7)',
+      disabled: 'rgba(255,255,255,0.5)',
+    },
+    error: {
+      main: '#f44335',
+      light: '#f6685c',
+      dark: '#aa2e24',
+      contrastText: '#fffffe',
     },
   },
 });
