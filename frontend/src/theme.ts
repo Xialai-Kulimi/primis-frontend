b791857e885b22cf453626e53a6c5ec86a194f4e
Kulimi <isilkoodarze@gmail.com>
2022-11-04 20:03:11 +0800

    Update src/theme.ts
    add mode: dark

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -3,7 +3,7 @@ import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
   palette: {
-    // type: 'dark',s
+    mode: 'dark',
     primary: {
       main: '#ff5252',
       light: '#ff7474',
