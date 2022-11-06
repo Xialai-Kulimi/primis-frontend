8a26ecb814f7a69aece86468520a2cc88c744dda
Kulimi <isilkoodarze@gmail.com>
2022-11-07 00:23:00 +0800

    Update Status.js
    import card.css

---
+++ frontend/src/view/Status.js
@@ -0,0 +1,11 @@
+import {Card} from '@mui/material'
+import '../css/card.css';
+
+function Status(){
+    return (
+        <Card variant='outlined' className="view-card">Text</Card>
+    )
+}
+
+export default Status
+
