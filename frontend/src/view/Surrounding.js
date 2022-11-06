1829bc43375ee99619d9b593ae013c02ca82469b
Kulimi <isilkoodarze@gmail.com>
2022-11-07 00:23:07 +0800

    Update Surrounding.js
    import card.css

---
+++ frontend/src/view/Surrounding.js
@@ -0,0 +1,35 @@
+import {Card} from '@mui/material'
+import '../css/card.css';
+
+function Surrounding(){
+    return (
+        <Card variant='outlined' className="view-card" style={{height: "100%"}}>
+            Textasdfasdfasdfasdfasdf <br/>
+            Textasdfasdfasdfasdfasdf <br/>
+            Textasdfasdfasdfasdfasdf <br/>
+            Textasdfasdfasdfasdfasdf <br/>
+            Textasdfasdfasdfasdfasdf <br/>
+            Textasdfasdfasdfasdfasdf <br/>
+            Textasdfasdfasdfasdfasdf <br/>
+            Textasdfasdfasdfasdfasdf <br/>
+            Textasdfasdfasdfasdfasdf <br/>
+            Textasdfasdfasdfasdfasdf <br/>
+            Textasdfasdfasdfasdfasdf <br/>
+            Textasdfasdfasdfasdfasdf <br/>
+            Textasdfasdfasdfasdfasdf <br/>
+            Textasdfasdfasdfasdfasdf <br/>
+            Textasdfasdfasdfasdfasdf <br/>
+            Textasdfasdfasdfasdfasdf <br/>
+            Textasdfasdfasdfasdfasdf <br/>
+            Textasdfasdfasdfasdfasdf <br/>
+            Textasdfasdfasdfasdfasdf <br/>
+            Textasdfasdfasdfasdfasdf <br/>
+            Textasdfasdfasdfasdfasdf <br/>
+            Textasdfasdfasdfasdfasdf <br/>
+            
+        </Card>
+    )
+}
+
+export default Surrounding
+
