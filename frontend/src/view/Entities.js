2691f33e7309f277526a7ef8ebecbcc62403d4ba
Kulimi <isilkoodarze@gmail.com>
2022-11-07 00:22:51 +0800

    Update Entities.js
    import card.css

---
+++ frontend/src/view/Entities.js
@@ -0,0 +1,12 @@
+import { Card } from '@mui/material'
+import '../css/card.css';
+
+function Entities() {
+    return (
+        <Card variant='outlined' className="view-card">
+            Text</Card>
+    )
+}
+
+export default Entities
+
