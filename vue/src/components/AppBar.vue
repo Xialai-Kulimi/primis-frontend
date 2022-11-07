b6f3830c9242e322296587d7467f4b5ef0c962f4
Kulimi <isilkoodarze@gmail.com>
2022-11-07 22:50:11 +0800

    feat: Crate AppBar

---
+++ vue/src/components/AppBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-card outlined style="padding-right: 10px">
+      <v-layout row align-center>
+        <v-card-title class="primary--text">PRIMIS</v-card-title>
+        <v-spacer></v-spacer>
+        <v-card-subtitle></v-card-subtitle>
+        <v-btn text color="primary">登出</v-btn>
+      </v-layout>
+    </v-card>
+
+  </v-container>
+</template>
+  
+<script>
+export default {
+  name: 'AppBar',
+
+  data: () => ({
+
+  }),
+}
+</script>
+  
