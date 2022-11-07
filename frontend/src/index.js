4b0f11bfeddaee909a3ef1fb6aba90d8c3b2b0cd
Kulimi <isilkoodarze@gmail.com>
2022-11-07 17:31:30 +0800

    feat: remove blank line

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
 
-
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 import { ThemeProvider } from '@mui/material/styles';
@@ -14,8 +13,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <App/>
+      <CssBaseline />
+        <App />
     </ThemeProvider>
 
   </React.StrictMode>
