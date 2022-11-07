6bd735cee2b852fe2aa2d5aaf65ad23385628ad9
Kulimi <isilkoodarze@gmail.com>
2022-11-07 17:30:48 +0800

    feat: Implement react component class

--- frontend/src/view/AppBar.js
+++ frontend/src/view/AppBar.js
@@ -1,35 +1,54 @@
-import {Card, Stack, Button, Typography} from '@mui/material'
+import { Card, Stack, Button, Typography } from '@mui/material'
+import React from 'react';
 
 
-function AppBar(prop){
-    var username = prop.username
-    var btn_text = ''
-    var disable = null
-    if (username !== "") {
-        btn_text = '登出' 
-        disable = false
-    }
-    else {
-        btn_text = '登入'
-        disable = true
-    }
 
+class AppBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { username: 0, btn_text: '登出', disable: null };
+    
+    
+    // if (this.state.username !== "") {
+    //   this.state.btn_text = '登出'
+    //   this.state.disable = false
+    // }
+    // else {
+    //   this.state.btn_text = '登入'
+    //   this.state.disable = true
+    // }
+  }
+  componentDidMount() {
+    let x = 0
+    setInterval(
+      function () {
+        x += 1
+        this.setState({
+          username: x
+        });
+      }.bind(this)
+      ,1000
+    );
+  };
+  render() {
     return (
-        <Card variant="outlined" style={{ marginTop: "10px", marginBottom: "30px"}}>
+      <Card variant="outlined" style={{ marginTop: "10px", marginBottom: "30px" }}>
 
-          <Stack direction="row-reverse">
+        <Stack direction="row-reverse">
 
-            <Button size="large" href="/api/auth/login" >{btn_text}</Button>
-            <Button size="large" varient="text" style={{ color: "white" }} disabled={disable}>
-              {username}
-            </Button>
-            <Typography variant='h4' color="primary" flexGrow="1" style={{marginLeft: "10px"}}>
-          PRIMIS
-        </Typography>
-          </Stack>
+          <Button size="large" href="/api/auth/login" >{this.state.btn_text}</Button>
+          <Button size="large" varient="text" style={{ color: "white" }} disabled={this.state.disable}>
+            {this.state.username}
+          </Button>
+          <Typography variant='h4' color="primary" flexGrow="1" style={{ marginLeft: "10px" }}>
+            PRIMIS
+          </Typography>
+        </Stack>
 
-        </Card>
+      </Card>
     )
+  }
 }
 
 export default AppBar
