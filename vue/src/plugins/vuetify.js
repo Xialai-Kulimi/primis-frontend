90bd6bd783047cc0db9ad317332139374a1df3a4
Kulimi <isilkoodarze@gmail.com>
2022-11-07 22:49:36 +0800

    feat: add some custom theme

--- vue/src/plugins/vuetify.js
+++ vue/src/plugins/vuetify.js
@@ -4,4 +4,15 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        dark: true,
+        themes: {
+          dark: {
+              primary: '#ff5252',
+              secondary:  '#770000',
+              accent: '#8c9eff',
+              error:  '#ff1400',
+          },
+        },
+      },
 });
