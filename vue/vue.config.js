b9e054397d2d84eb8b694f8dffd9e68616e8426c
Kulimi <isilkoodarze@gmail.com>
2022-11-07 22:49:10 +0800

    feat: add devserver proxy and public path

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,6 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: '/primis',
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    allowedHosts: [".tw"],
+    proxy: {
+      '/api/auth': {
+        target: 'http://localhost:9002/'
+      }
+    }
+  },
 })
