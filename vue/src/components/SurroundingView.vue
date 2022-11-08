ad5c79f3f980fa978a67fed53979f1139a237ec2
Kulimi <isilkoodarze@gmail.com>
2022-11-08 18:01:20 +0800

    feat: link to store

--- vue/src/components/SurroundingView.vue
+++ vue/src/components/SurroundingView.vue
@@ -1,9 +1,29 @@
 <template>
-    <v-card outlined height="40vh">surrounding</v-card>
+    <v-card outlined height="40vh">
+      <v-layout column reverse fill-height>
+      <v-container style="padding: 0">
+        <v-text-field 
+          dense 
+          solo 
+          hide-details
+          label="說些什麼吧"></v-text-field>
+      </v-container>
+
+      <v-card-text style="padding-bottom: 0">
+        <div v-for="item in surrounding" :key="item" :class="item.class">{{ item.message }}</div>
+      </v-card-text>
+
+    </v-layout>
+    </v-card>
 </template>
 <script>
 export default {
   name: 'SurroundingView',
+  computed: {
+    surrounding() {
+      return this.$store.state.surrounding;
+    }
+  },
 
 };
 </script>
