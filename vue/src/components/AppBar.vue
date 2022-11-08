6cfd51aa993117cd13173c9926983fbd28624c72
Xialai-Kulimi <isilkoodarze@gmail.com>
2022-11-08 14:06:46 +0800

    feat: change prmis font

--- vue/src/components/AppBar.vue
+++ vue/src/components/AppBar.vue
@@ -2,7 +2,7 @@
   <v-container>
     <v-card outlined style="padding-right: 10px">
       <v-layout row align-center>
-        <v-card-title class="primary--text">PRIMIS</v-card-title>
+        <v-card-title class="primary--text"><pre>PRIMIS</pre></v-card-title>
         <v-spacer></v-spacer>
         <v-card-subtitle>{{user.username}}</v-card-subtitle>
         <v-btn text color="primary">登出</v-btn>
