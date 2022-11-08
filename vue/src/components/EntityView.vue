b88ccd443ff93fe4dc2526153673358a0729cd32
Xialai-Kulimi <isilkoodarze@gmail.com>
2022-11-08 13:22:26 +0800

    refactor: vue component should be multi words

--- vue/src/components/EntityView.vue
+++ vue/src/components/EntityView.vue
@@ -3,7 +3,7 @@
 </template>
 <script>
 export default {
-  name: 'Entity',
+  name: 'EntityView',
 
 };
 </script>
--- vue/src/components/SurroundingView.vue
+++ vue/src/components/SurroundingView.vue
@@ -3,7 +3,7 @@
 </template>
 <script>
 export default {
-  name: 'Surrounding',
+  name: 'SurroundingView',
 
 };
 </script>
