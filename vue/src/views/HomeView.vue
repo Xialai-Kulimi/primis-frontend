ec4c66d98b8a562160e3674b3b5807d15fe19c04
Xialai-Kulimi <isilkoodarze@gmail.com>
2022-11-08 11:04:07 +0800

    feat: add some component

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,6 +1,41 @@
 <template>
   <v-container>
-    <v-alert type="error" outlined>{{user.id}}</v-alert>
+    <v-alert 
+      :type="local_operation.alert.color" 
+      dismissible outlined>{{local_operation.alert.text}}</v-alert>
+    <v-snackbar
+      v-model="local_operation.snackbar"
+      :color="local_operation.snackbar.color"
+    >
+      {{ local_operation.snackbar.text }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          color="primary"
+          text
+          v-bind="attrs"
+          @click="local_operation.snackbar = false"
+        >
+          關閉
+        </v-btn>
+      </template>
+    </v-snackbar>
+    <v-dialog
+      v-model="local_operation.dialog"
+    >
+      <v-card outlined>
+        <v-card-title>{{local_operation.dialog.title}}</v-card-title>
+        <v-card-text>{{local_operation.dialog.text}}</v-card-text>
+      </v-card>
+    </v-dialog>
+    <v-dialog
+      v-model="local_operation.input"
+    >   
+
+      <template v-for="i in local_operation.input" >
+        <v-input :key="i">{{i}}</v-input>
+      </template>
+    </v-dialog>
     <v-row dense>
       <v-col cols="12" sm="6">
         <Caption></Caption>
@@ -29,8 +64,23 @@ import Status from '@/components/Status';
 export default {
   name: 'Home',
   data: ()=>{
-    local_operation: {
+    return {
+      local_operation: {
+      alert: {
+        text: '',
+        color: ''
+      },
+      dialog: {
+        title: '',
+        text: ''
+      },
+      snackbar: {
+        text: '',
+        color: ''
+      },
+      input: []
 
+    }
     }
   },
   computed: {
@@ -41,7 +91,9 @@ export default {
   watch: {
     operation: {
       handler: function () {
-        
+        for (let key in this.operation){
+          this.local_operation[key] = this.operation[key]
+        }
       },
     },
   },
