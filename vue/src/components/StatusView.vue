9b0e8e4884c9ad0e91017155f95bc0a1297c548d
Kulimi <isilkoodarze@gmail.com>
2022-11-08 18:01:12 +0800

    feat: link to store

--- vue/src/components/StatusView.vue
+++ vue/src/components/StatusView.vue
@@ -1,11 +1,31 @@
 <template>
-    <v-card outlined height="20vh"><div>status</div></v-card>
+    <v-card outlined height="20vh">
+      <v-layout column reverse fill-height>
+      <v-container style="padding: 0">
+        <v-text-field 
+          dense 
+          solo 
+          hide-details
+          label="說些什麼吧"></v-text-field>
+      </v-container>
+
+      <v-card-text style="padding-bottom: 0">
+        <div v-for="item in status" :key="item" :class="item.class">{{ item.message }}</div>
+      </v-card-text>
+
+    </v-layout>
+    </v-card>
 </template>
 <script>
 
 
 export default {
   name: 'StatusView',
+  computed: {
+    status() {
+      return this.$store.state.status;
+    }
+  },
 
 };
 </script>
