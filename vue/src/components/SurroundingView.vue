dad5b6392cfc85afe35bb939ef68086a93f23bba
Kulimi <isilkoodarze@gmail.com>
2022-11-09 21:41:39 +0800

    perf: remove useless key

--- vue/src/components/SurroundingView.vue
+++ vue/src/components/SurroundingView.vue
@@ -4,7 +4,7 @@
 
 
       <v-card-text style="padding-bottom: 0;overflow: auto;">
-        <div v-for="item in surrounding" :key="item" :class="item.class">{{ item.message }}</div>
+        <div v-for="item in surrounding" :class="item.class">{{ item.message }}</div>
       </v-card-text>
 
     </v-layout>
