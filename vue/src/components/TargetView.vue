51ff6b116f69c0ba89c534f2d28e7ea848ca29c8
Kulimi <isilkoodarze@gmail.com>
2022-11-09 14:14:48 +0800

    feat: render targets

--- vue/src/components/TargetView.vue
+++ vue/src/components/TargetView.vue
@@ -1,11 +1,31 @@
 <template>
-    <v-card outlined height="20vh">Target</v-card>
+  <v-card outlined height="20vh">
+    <v-card-text>
+      <template v-for="(button, index) in target">
+        <v-btn :color="button.color">{{button.text}}</v-btn>
+        <v-menu >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn :color="button.color" v-bind="attrs" v-on="on" icon>
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </template>
+          
+          <v-list>
+          <v-list-item v-for="(item, index) in button.list" :key="index">
+            <v-list-item-title>{{ item.text }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </template>
+
+    </v-card-text>
+  </v-card>
 </template>
 <script>
 export default {
   name: 'TargetView',
   computed: {
-    caption() {
+    target() {
       return this.$store.state.target;
     }
   },
