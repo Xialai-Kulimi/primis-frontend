36874c0238d5feafbff5e4f1202c164c914ac4a0
Kulimi <isilkoodarze@gmail.com>
2022-11-09 14:48:47 +0800

    fix: fix button wrap ugly

--- vue/src/components/TargetView.vue
+++ vue/src/components/TargetView.vue
@@ -1,23 +1,23 @@
 <template>
   <v-card outlined height="20vh">
     <v-card-text>
-      <template v-for="(button, index) in target">
-        <v-btn :color="button.color">{{button.text}}</v-btn>
-        <v-menu >
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn :color="button.color" v-bind="attrs" v-on="on" icon>
-              <v-icon>mdi-dots-vertical</v-icon>
-            </v-btn>
-          </template>
-          
-          <v-list>
-          <v-list-item v-for="(item, index) in button.list" :key="index">
-            <v-list-item-title>{{ item.text }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </template>
-
+      <v-layout row>
+        <v-form v-for="(button, index) in target" :key="index" style="padding: 2px">
+          <v-btn :color="button.color">{{ button.text }}</v-btn>
+          <v-menu>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn :color="button.color" v-bind="attrs" v-on="on" icon>
+                <v-icon>mdi-dots-vertical</v-icon>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item v-for="(item, index) in button.list" :key="index">
+                <v-list-item-title>{{ item.text }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-form>
+      </v-layout>
     </v-card-text>
   </v-card>
 </template>
