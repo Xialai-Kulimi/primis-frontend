5d8fa4ecc793f078f283eeb29b10364dc5ad6268
Kulimi <isilkoodarze@gmail.com>
2022-11-09 16:36:55 +0800

    feat: add rich console to ws_test

--- test_ws/main.py
+++ test_ws/main.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
+from rich.console import Console
+console = Console()
 
 app = FastAPI()
 
 
-
-
-
 @app.websocket("/api/ws")
 async def websocket_endpoint(websocket: WebSocket):
+    console.log('connect')
     await websocket.accept()
     while True:
         data = await websocket.receive_text()
+        console.print(data)
         await websocket.send_text(f"Message text was: {data}")
