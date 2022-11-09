205b5b8b38d2d23524ee8254cafa4a09d95ff029
Kulimi <isilkoodarze@gmail.com>
2022-11-09 22:46:41 +0800

    test: add btn tests

--- test_ws/main.py
+++ test_ws/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
 import asyncio
 from rich.console import Console
+from datetime import datetime
 console = Console()
 
 app = FastAPI()
@@ -22,16 +23,20 @@ class ConnectionManager:
 
 manager = ConnectionManager()
 
+
 async def main():
     while True:
         await asyncio.sleep(1)
         console.log(manager.active_connections)
         for i in manager.active_connections:
-            await i.send_json({'type': 'surrounding', 'message': [{'class': 'info--text', 'message': '又一秒過去了'},]})
+            await i.send_json({'type': 'surrounding', 'message': [{'class': 'info--text', 'message': '又一秒過去了'}, ]})
+            await i.send_json({'type': 'status', 'message': [{'class': 'info--text', 'message': str(datetime.now())}, ]})
+
 
 @app.on_event("startup")
 async def startup_event():
     asyncio.create_task(main())
+
 
 @app.websocket("/api/ws")
 async def websocket_endpoint(websocket: WebSocket):
@@ -41,10 +46,60 @@ async def websocket_endpoint(websocket: WebSocket):
 
             data = await websocket.receive_json()
             console.log(data)
-            await websocket.send_json({'type': 'caption', 'message': [{'class': '', 'message': f'你說：「{data["content"]}」'},]})
-            await websocket.send_json({'type': 'caption', 'message': [{'class': 'primary--text', 'message': 'primary color test'},]})
-            await websocket.send_json({'type': 'surrounding', 'message': [{'class': 'primary--text', 'message': 'primary color test'},]})
-            await websocket.send_json({'type': 'status', 'message': [{'class': 'primary--text', 'message': 'primary color test'},]})
+            await websocket.send_json({'type': 'caption', 'message': [{'class': '', 'message': f'你說：「{data["content"]}」'}, ]})
+            await websocket.send_json({'type': 'caption', 'message': [{'class': 'primary--text', 'message': 'primary color test'}, ]})
+            await websocket.send_json({'type': 'surrounding', 'message': [{'class': 'primary--text', 'message': 'primary color test'}, ]})
+            await websocket.send_json({'type': 'status', 'message': [{'class': 'primary--text', 'message': 'primary color test'}, ]})
+            await websocket.send_json({
+                'type': 'target',
+                'buttons': [
+                    {
+                        'text': 'text1',
+                        'id': 'btn1',
+                        'color': 'primary',
+                        'list': [
+                            {'text': 'text', 'value': 'value'},
+                            {'text': 'text2', 'value': 'value2'},
+                            {'text': 'text3', 'value': 'value3'},
+                        ]
+                    },
+                    {
+                        'text': 'text2',
+                        'id': 'btn2',
+                        'color': 'error',
+                        'list': [
+                            {'text': 'text', 'value': 'value'},
+                            {'text': 'text2', 'value': 'value2'},
+                            {'text': 'text3', 'value': 'value3'},
+                        ]
+                    },
+                ]
+            })
+            await websocket.send_json({
+                'type': 'reachable',
+                'buttons': [
+                    {
+                        'text': 'text1',
+                        'id': 'btn3231',
+                        'color': 'primary',
+                        'list': [
+                            {'text': 'text', 'value': 'value'},
+                            {'text': 'text2', 'value': 'value2'},
+                            {'text': 'text3', 'value': 'value3'},
+                        ]
+                    },
+                    {
+                        'text': 'text2',
+                        'id': 'btn22323',
+                        'color': 'info',
+                        'list': [
+                            {'text': 'text', 'value': 'value78'},
+                            {'text': 'text2', 'value': 'value27878'},
+                            {'text': 'text3', 'value': 'value37878'},
+                        ]
+                    },
+                ]
+            })
             # await manager.send_personal_message(f"You wrote: {data}", websocket)
             # await manager.broadcast(f"Client #{client_id} says: {data}")
     except WebSocketDisconnect:
