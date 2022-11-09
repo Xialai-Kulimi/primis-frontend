cbe0b77f7e577a3b226e2343d26015c00655e540
Kulimi <isilkoodarze@gmail.com>
2022-11-09 15:32:37 +0800

    feat: update about content

--- vue/src/views/AboutView.vue
+++ vue/src/views/AboutView.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container>
+    <v-card outlined>
+      <v-card-title>
+        <pre>PRIMIS</pre>
+      </v-card-title>
+      <v-card-text>
+        不知道為什麼，當我回過神來的時候，我正傾注全部的精力編織一個美麗的世界。
+      </v-card-text>
+
+    </v-card>
+  </v-container>
 </template>
