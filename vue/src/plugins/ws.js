8ffb2b3be4806a6a8d865e44cacab318fa76b991
ItisCaleb <40360126+ItisCaleb@users.noreply.github.com>
2022-11-11 15:28:58 +0800

    feat: add ws reconnect

--- vue/src/plugins/ws.js
+++ vue/src/plugins/ws.js
@@ -3,7 +3,8 @@ import {mapMutations} from "vuex";
 export const WS = {
     data(){
         return{
-            ws:null
+            ws:null,
+            timeout: 250
         }
     },
     methods:{
@@ -11,11 +12,18 @@ export const WS = {
         initWebsocket(){
             let url = `ws://${window.location.host}/api/ws`
             this.ws = new WebSocket(url)
-            this.ws.onopen = ()=>console.log("ws connected")
+            this.ws.onopen = ()=>{
+                this.timeout = 250
+                console.log("ws connected")
+            }
             this.ws.onerror = (e)=>console.error("ws connection fail",e)
             this.ws.onmessage = (e)=>{
                 let _data = e.data;
                 this.setWsData(_data)
+            }
+            this.ws.onclose = ()=>{
+                console.log("ws reconnecting...")
+                setTimeout(this.initWebsocket,Math.min(10000,this.timeout+=this.timeout))
             }
         },
         wsSend(data){
