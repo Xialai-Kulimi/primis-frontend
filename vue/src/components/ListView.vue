a80df2f037564c2d0c0431a0d5026a81ea730f56
Xialai-Kulimi <isilkoodarze@gmail.com>
2022-11-13 02:29:05 +0800

    perf: add lazy and some hover style

--- vue/src/components/ListView.vue
+++ vue/src/components/ListView.vue
@@ -4,48 +4,59 @@
     :height="height"
     style="overflow-x: auto; flex-wrap: wrap"
   >
-    
-      <v-list v-if="buttons.length !== 0">
-        <v-list-item v-for="(button, index) in buttons" :key="index">
-          <v-dialog v-model="button.dialog" width="500">
-            <template v-slot:activator="{ on, attrs }">
-              <v-list-content v-bind="attrs" v-on="on">
+    <v-lazy
+      transition="fade-transition"
+      :options="{
+        threshold: 0.5,
+      }"
+    >
+      <v-list v-if="buttons.length !== 0" flat>
+        <v-dialog
+          v-for="(button, index) in buttons"
+          :key="index"
+          v-model="button.dialog"
+          width="500"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-list-item v-bind="attrs" v-on="on">
+              <v-list-item-content>
                 <v-list-item-title :class="button.color + '--text'">{{
                   button.text
                 }}</v-list-item-title>
                 <v-list-item-subtitle>
                   {{ button.description }}
                 </v-list-item-subtitle>
-              </v-list-content>
-            </template>
+              </v-list-item-content>
+              <v-spacer></v-spacer>
 
-            <v-card outlined>
-              <v-card-title :class="button.color + '--text'">
-                {{ button.text }}
-              </v-card-title>
-                <v-card-subtitle>{{ button.description }}</v-card-subtitle>
-              <v-card-text>
-                <v-list>
-  
-              <v-list-item  v-for="(item, index) in button.list" :key="index">
-                <v-list-item-title @click="SubmitClick(button.id, item.value)"
-                  >{{ item.text }}
-                </v-list-item-title>
-              </v-list-item>
-            </v-list>
-              </v-card-text>
-            </v-card>
-          </v-dialog>
+              <v-list-item-action>
+                <v-btn icon @click.stop.prevent="SubmitClick(button.id, 'default')">
+                  <v-icon color="grey lighten-1">mdi-walk</v-icon>
+                </v-btn>
+              </v-list-item-action>
+            </v-list-item>
+          </template>
 
-          <v-spacer></v-spacer>
-
-          <v-list-item-action>
-            <v-btn icon @click="SubmitClick(button.id, 'default')">
-              <v-icon color="grey lighten-1">mdi-walk</v-icon>
-            </v-btn>
-          </v-list-item-action>
-        </v-list-item>
+          <v-card outlined>
+            <v-card-title :class="button.color + '--text'">
+              {{ button.text }}
+            </v-card-title>
+            <v-card-subtitle>{{ button.description }}</v-card-subtitle>
+            <v-card-text>
+              <v-list>
+                <v-list-item
+                  v-for="(item, index) in button.list"
+                  :key="index"
+                  @click="SubmitClick(button.id, item.value)"
+                >
+                  <v-list-item-title>{{ item.text }} </v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-card-text>
+          </v-card>
+        </v-dialog>
       </v-list>
+    </v-lazy>
   </v-card>
 </template>
 <script>
