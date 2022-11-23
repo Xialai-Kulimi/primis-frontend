0bb2190702ea88932a73bad73379cc1f40aae19f
ItisCaleb <40360126+ItisCaleb@users.noreply.github.com>
2022-11-23 22:22:30 +0800

    feat: update tab

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -79,42 +79,33 @@
       <!---->
       <v-card-text>
         <v-row dense>
-          <v-col cols="12" sm="6">
-            <v-tabs v-model="tab">
-              <v-tab v-for="(icon, index) in ['mdi-walk', 'mdi-account-multiple-outline', 'mdi-map-marker-alert-outline']" :key="index">
-                <v-icon>{{ icon }}</v-icon>
+          <v-col>
+            <v-tabs v-model="tab" >
+              <v-tab  v-for="(icon, index) in ['mdi-walk', 'mdi-account-multiple-outline', 'mdi-map-marker-alert-outline']" :key="index">
+                <v-icon>{{icon}}</v-icon>
               </v-tab>
             </v-tabs>
             <v-tabs-items v-model="tab">
-              <v-tab-item :transition="false" key="0">
-                <ListView :height="left_view_height" type="target" />
-              </v-tab-item>
-              <v-tab-item :transition="false" key="1">
-                <ListView :height="left_view_height" type="player" />
-              </v-tab-item>
-              <v-tab-item :transition="false" key="2">
-                <ListView :height="left_view_height" type="reachable" />
-              </v-tab-item>
+              <v-tab-item :transition="false" key="0"><ListView :height="left_view_height" type="target" /></v-tab-item>
+              <v-tab-item :transition="false" key="1"><ListView :height="left_view_height" type="player" /></v-tab-item>
+              <v-tab-item :transition="false" key="2"><BtnsView :height="left_view_height" type="inventory" /></v-tab-item>
             </v-tabs-items>
           </v-col>
-          <v-col cols="12" sm="6">
-            <v-tabs v-model="sub_tab">
+          <v-col>
+            <ListView :height="right_text_height" type="reachable" />
+            <v-divider></v-divider>
+            <v-tabs v-model="sub_tab" >
               <v-tab v-for="(icon, index) in ['mdi-chat-processing-outline', 'mdi-panorama-outline']" :key="index">
-                <v-icon>{{ icon }}</v-icon>
+                <v-icon>{{icon}}</v-icon>
               </v-tab>
             </v-tabs>
             <v-tabs-items v-model="sub_tab">
-              <v-tab-item :transition="false" key="0">
-                <TextsView :height="right_text_height" type="caption" :input="true" />
-              </v-tab-item>
-              <v-tab-item :transition="false" key="1">
-                <TextsView :height="right_text_height" type="surrounding" />
-              </v-tab-item>
+              <v-tab-item :transition="false" key="0"><TextsView :height="right_btn_height" type="caption" :input="true" /></v-tab-item>
+              <v-tab-item :transition="false" key="1"><TextsView :height="right_btn_height" type="status" /></v-tab-item>
+              <v-tab-item :transition="false" key="2"><ListView :height="right_btn_height" type="reachable" /></v-tab-item>
             </v-tabs-items>
-            <BtnsView :height="right_btn_height" type="inventory" />
           </v-col>
         </v-row>
-
       </v-card-text>
     </v-card>
     <!-- <v-row dense>
