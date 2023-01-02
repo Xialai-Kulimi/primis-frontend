e738c12777335933a5364776aa794f7d33a85a99
Xialai-Kulimi <isilkoodarze@gmail.com>
2023-01-02 20:30:10 +0800

    feat: imeplement reconnect button

--- vue/src/components/DisconnectOverlay.vue
+++ vue/src/components/DisconnectOverlay.vue
@@ -10,7 +10,7 @@
           </v-card-title>
           <v-card-subtitle>如果遲遲無法連線可以嘗試重新載入此分頁，並且避免同時在多個頁面登入。</v-card-subtitle>
 
-          <v-btn color="primary" text block>立即重新連線</v-btn>
+          <v-btn color="primary" text block @click="initWebsocket()">立即重新連線</v-btn>
 
         </v-alert>
         <v-progress-linear indeterminate color="primary" size="64"></v-progress-linear>
@@ -18,9 +18,11 @@
     </v-overlay>
 </template>
 <script>
+import { WS } from "@/plugins/ws";
 
 
 export default {
+  mixins: [WS],
   name: 'DisconnectOverlay',
   
   data: () => ({
