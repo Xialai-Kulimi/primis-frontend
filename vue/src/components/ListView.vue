dbd0595a9c92db5c0c63645f3f38f3091c7ae4ac
Xialai-Kulimi <isilkoodarze@gmail.com>
2023-01-09 01:42:05 +0800

    style: formatting

--- vue/src/components/ListView.vue
+++ vue/src/components/ListView.vue
@@ -1,5 +1,9 @@
 <template>
-  <v-card elevation="0" :height="height" style="overflow-x: auto; flex-wrap: wrap">
+  <v-card
+    elevation="0"
+    :height="height"
+    style="overflow-x: auto; flex-wrap: wrap"
+  >
     <v-dialog v-model="dialog" width="500">
       <v-card outlined v-if="dialog_button">
         <v-card-title :class="dialog_button.style">
@@ -8,43 +12,55 @@
         <v-card-subtitle>{{ dialog_button.description }}</v-card-subtitle>
         <v-card-text>
           <v-list>
-            <v-list-item v-for="(item, index2) in dialog_button.list" :key="index2"
-              @click="SubmitClick(dialog_button.id, item.value)" :disabled="item.disabled">
+            <v-list-item
+              v-for="(item, index2) in dialog_button.list"
+              :key="index2"
+              @click="SubmitClick(dialog_button.id, item.value)"
+              :disabled="item.disabled"
+            >
               <v-list-item-title>{{ item.text }} </v-list-item-title>
             </v-list-item>
           </v-list>
         </v-card-text>
       </v-card>
     </v-dialog>
-    <v-lazy transition="fade-transition" :options="{
-  threshold: 0.5,
-}">
+    <v-lazy
+      transition="fade-transition"
+      :options="{
+        threshold: 0.5,
+      }"
+    >
       <v-list v-if="buttons.length !== 0" flat>
-
-        <v-list-item v-for="(button, index) in buttons" @click="OpenDialog(button)" :disabled="button.disabled"
-          :key="index">
+        <v-list-item
+          v-for="(button, index) in buttons"
+          @click="OpenDialog(button)"
+          :disabled="button.disabled"
+          :key="index"
+        >
           <v-list-item-content>
             <v-list-item-title :class="button.style">{{
-    button.text
-}}</v-list-item-title>
+              button.text
+            }}</v-list-item-title>
             <v-list-item-subtitle>
               {{ button.description }}
             </v-list-item-subtitle>
           </v-list-item-content>
 
           <v-list-item-action v-if="button.value">
-            <v-btn text rounded @click.stop.prevent="SubmitClick(button.id, button.value)" class="text-caption"
-              color="grey lighten-1">
+            <v-btn
+              text
+              rounded
+              @click.stop.prevent="SubmitClick(button.id, button.value)"
+              class="text-caption"
+              color="grey lighten-1"
+            >
               {{ button.action }}
               <v-icon color="grey lighten-1">{{ button.icon }}</v-icon>
             </v-btn>
-
           </v-list-item-action>
         </v-list-item>
-
       </v-list>
     </v-lazy>
-
   </v-card>
 </template>
 <script>
@@ -64,16 +80,16 @@ export default {
       );
     },
     OpenDialog(button) {
-      this.dialog_button = structuredClone(button)
-      this.dialog = true
-    }
+      this.dialog_button = structuredClone(button);
+      this.dialog = true;
+    },
   },
   data: () => {
     return {
       dialog: false,
-      dialog_button: {}
+      dialog_button: {},
     };
   },
-  mounted: () => { },
+  mounted: () => {},
 };
 </script>
--- vue/src/plugins/ws.js
+++ vue/src/plugins/ws.js
@@ -1,50 +1,62 @@
 import { mapMutations } from "vuex";
 
 export const WS = {
-    data() {
-        return {
-            ws: null,
-            timeout: 250
-        }
-    },
-    methods: {
-        ...mapMutations(['setWsData', 'setConnectState', 'setOperation', 'pushTexts', 'setButtons']),
-        initWebsocket() {
-            // if ws is CONNECTING or OPEN then don't initialize ws
-            if (this.ws && (this.ws.readyState == 0 || this.ws.readyState == 1)) {
-                return
-            }
+  data() {
+    return {
+      ws: null,
+      timeout: 250,
+    };
+  },
+  methods: {
+    ...mapMutations([
+      "setWsData",
+      "setConnectState",
+      "setOperation",
+      "pushTexts",
+      "setButtons",
+    ]),
+    initWebsocket() {
+      // if ws is CONNECTING or OPEN then don't initialize ws
+      if (this.ws && (this.ws.readyState == 0 || this.ws.readyState == 1)) {
+        return;
+      }
 
-            let url = `wss://${window.location.host}/api/ws`
-            this.ws = new WebSocket(url)
-            this.ws.onopen = () => {
-                this.timeout = 2500
-                this.setConnectState(true)
-                console.log("ws connected")
-            }
-            this.ws.onerror = (e) => { console.error("ws connection fail", e); this.setConnectState(false) }
-            this.ws.onmessage = (e) => {
-                let _data = e.data;
-                let recv = JSON.parse(_data)
-                // console.log(recv.type)
-                if (recv.type === 'operation') {
-                    this.setOperation(recv)
-                }
-                else if (['caption', 'surrounding', 'status'].includes(recv.type)) {
-                    this.pushTexts(recv)
-                }
-                else if (['target', 'reachable', 'inventory', 'player'].includes(recv.type)) {
-                    this.setButtons(recv)
-                }
-            }
-            this.ws.onclose = () => {
-                this.setConnectState(false)
-                console.log("ws reconnecting...")
-                setTimeout(this.initWebsocket, Math.max(5000, this.timeout += this.timeout))
-            }
-        },
-        wsSend(data) {
-            this.ws.send(data)
+      let url = `wss://${window.location.host}/api/ws`;
+      this.ws = new WebSocket(url);
+      this.ws.onopen = () => {
+        this.timeout = 2500;
+        this.setConnectState(true);
+        console.log("ws connected");
+      };
+      this.ws.onerror = (e) => {
+        console.error("ws connection fail", e);
+        this.setConnectState(false);
+      };
+      this.ws.onmessage = (e) => {
+        let _data = e.data;
+        let recv = JSON.parse(_data);
+        // console.log(recv.type)
+        if (recv.type === "operation") {
+          this.setOperation(recv);
+        } else if (["caption", "surrounding", "status"].includes(recv.type)) {
+          this.pushTexts(recv);
+        } else if (
+          ["target", "reachable", "inventory", "player"].includes(recv.type)
+        ) {
+          this.setButtons(recv);
         }
-    }
-}
+      };
+      this.ws.onclose = () => {
+        this.setConnectState(false);
+        console.log("ws reconnecting...");
+        setTimeout(
+          this.initWebsocket,
+          Math.max(5000, (this.timeout += this.timeout))
+        );
+      };
+    },
+    wsSend(data) {
+      this.ws.send(data);
+    },
+  },
+};
