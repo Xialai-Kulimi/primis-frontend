576cfc80644cbcbdbc3cd479f2ab5e716198f004
Xialai-Kulimi <isilkoodarze@gmail.com>
2023-01-08 20:28:55 +0800

    feat: create ListItemIconLoader for custom skeleton loader

---
+++ vue/src/components/ListItemIconLoader.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-list-item :ripple="false">
+    <v-list-item-icon>
+      <v-progress-circular
+        indeterminate
+        color="grey darken-3"
+      ></v-progress-circular>
+    </v-list-item-icon>
+    <v-list-item-content>
+      <v-list-item-title>
+        <v-skeleton-loader type="text"></v-skeleton-loader>
+      </v-list-item-title>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+<script>
+export default {
+  name: "ListItemIconLoader",
+};
+</script>
