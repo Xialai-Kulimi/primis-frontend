fcf8873f4b7130d0b6347cdac5cc44f71d036f1a
Xialai-Kulimi <isilkoodarze@gmail.com>
2023-01-08 20:29:26 +0800

    feat: add ListItemIconLoader when loading auth0

--- vue/src/components/LeftSideBarAppend.vue
+++ vue/src/components/LeftSideBarAppend.vue
@@ -1,78 +1,66 @@
 <template>
-    <v-list>
-        <!-- <div>
-            
-            <button v-if="!$auth.isAuthenticated" @click="login">Log in</button>
-            
-            <button v-if="$auth.isAuthenticated" @click="logout">Log out</button>
-        </div> -->
-        <template v-if="!$auth.loading">
-            <template v-if="$auth.isAuthenticated">
-                <UserInfo></UserInfo>
+  <v-list>
+    <template v-if="!$auth.loading">
+      <template v-if="$auth.isAuthenticated">
+        <UserInfo></UserInfo>
 
-                <v-divider></v-divider>
+        <v-divider></v-divider>
 
-                <v-list-item @click="logout" :ripple="false">
-                    <v-list-item-icon>
-                        <v-icon color="primary">mdi-logout-variant</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-content>
-                        <v-list-item-title>登出</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-            </template>
-            <template v-else>
-
-                <v-list-item @click="login" :ripple="false">
-                    <v-list-item-icon>
-                        <v-icon color="primary">mdi-logout-variant</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-content>
-                        <v-list-item-title>登入</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-
-            </template>
-
-            <v-divider></v-divider>
-        </template>
-        <template v-else>
-
-        </template>
-
-        <v-list-item @click="setting" :ripple="false">
-            <v-list-item-icon>
-                <v-icon color="primary">mdi-cog-outline</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-                <v-list-item-title>設定</v-list-item-title>
-            </v-list-item-content>
+        <v-list-item @click="logout" :ripple="false">
+          <v-list-item-icon>
+            <v-icon color="primary">mdi-logout-variant</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>登出</v-list-item-title>
+          </v-list-item-content>
         </v-list-item>
+      </template>
+      <template v-else>
+        <v-list-item @click="login" :ripple="false">
+          <v-list-item-icon>
+            <v-icon color="primary">mdi-logout-variant</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>登入</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
 
-    </v-list>
+      <v-divider></v-divider>
+    </template>
+    <template v-else>
+        <ListItemIconLoader></ListItemIconLoader>
+    </template>
+
+    <v-list-item @click="setting" :ripple="false">
+      <v-list-item-icon>
+        <v-icon color="primary">mdi-cog-outline</v-icon>
+      </v-list-item-icon>
+      <v-list-item-content>
+        <v-list-item-title>設定</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
 </template>
 <script>
-import UserInfo from '@/components/UserInfo';
+import UserInfo from "@/components/UserInfo";
+import ListItemIconLoader from "@/components/ListItemIconLoader";
 
 export default {
-    name: 'LeftSideBarAppend',
-    components: { UserInfo },
-    data: () => ({
-
-    }),
-    methods: {
-
-        setting: function () { },
-        login() {
-            this.$auth.loginWithRedirect();
-        },
-        // Log the user out
-        logout() {
-            this.$auth.logout({
-                returnTo: window.location.origin
-            });
-        }
-    }
-
+  name: "LeftSideBarAppend",
+  components: { UserInfo, ListItemIconLoader },
+  data: () => ({}),
+  methods: {
+    setting: function () {},
+    login() {
+      this.$auth.loginWithRedirect();
+    },
+    // Log the user out
+    logout() {
+      this.$auth.logout({
+        returnTo: window.location.origin,
+      });
+    },
+  },
 };
 </script>
